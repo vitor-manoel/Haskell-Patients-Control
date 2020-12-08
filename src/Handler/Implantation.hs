8f012d7669a0d8b72f6a338b0868c1619830584a
Caroline Moura <carolinedemouraa@gmail.com>
2020-12-07 22:49:36 -0300

    Deploy heroku test

--- src/Handler/Implantation.hs
+++ src/Handler/Implantation.hs
@@ -8,14 +8,3 @@ module Handler.Implantation where
 
 import Import
 
-formImplantation :: Form (Implantation, Text)
-formImplantation = undefined
-
-getImplantationR :: Maybe PatientId -> Handler Html
-getImplantationR = undefined
-
-postImplantationR :: PatientId -> Handler Html
-postImplantationR = undefined
-
-postIDeleteR :: ImplantationId -> Handler Html
-postIDeleteR = undefined
