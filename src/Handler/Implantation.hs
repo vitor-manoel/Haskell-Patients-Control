72a8b18d6cb8ac12bb5d5750528e65711f3ec306
Caroline Moura <carolinedemouraa@gmail.com>
2020-12-07 21:59:50 -0300

    implantation sstage

--- src/Handler/Implantation.hs
+++ src/Handler/Implantation.hs
@@ -7,3 +7,20 @@
 module Handler.Implantation where
 
 import Import
+
+formImplantation :: Maybe Implantation -> Form Implantation
+formImplantation mp = renderDivs $ Implantation
+    <$> areq textField "Frequência: " (fmap ImplantationFraquency mp)
+    <*> areq textField "Observação: " (fmap ImplantationObservation mp)
+
+getImplantationR :: ImplantationId -> Handler Html
+getImplantationR iid = do
+    (widget,_) <- generateFormPost formLogin
+    msg <- getMessage
+    defaultLayout $ do
+        ^{widget}                    
+
+postIDeleteR :: ImplantationId -> Handler Html
+postIDeleteR iid = do
+    runDB $ delete iid
+    redirect PListR
--- src/Handler/Patient.hs
+++ src/Handler/Patient.hs
@@ -87,7 +87,7 @@ getPListR = do
                         <td>
                             <form action=@{PDeleteR pid} method=post>
                                 <input type="submit" value="X">
-        <form to={"/patient"}> 
+        <form action=@{PatientR} method=get>
             <input type="submit" value="Cadastrar">
                         
     |]
