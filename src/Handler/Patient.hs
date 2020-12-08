8a00d24324d058238a5562d14f0cfb963c49904a
Caroline Moura <carolinedemouraa@gmail.com>
2020-12-07 23:01:42 -0300

    Revert "implantation sstage"
    This reverts commit 72a8b18d6cb8ac12bb5d5750528e65711f3ec306.

--- src/Handler/Patient.hs
+++ src/Handler/Patient.hs
@@ -87,8 +87,7 @@ getPListR = do
                         <td>
                             <form action=@{PDeleteR pid} method=post>
                                 <input type="submit" value="X">
-        <form action=@{PatientR} method=get>
-            <input type="submit" value="Cadastrar">
+        <input href=@{PatientR} value="Cadastrar">
                         
     |]
 
